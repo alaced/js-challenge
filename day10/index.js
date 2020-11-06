6fc8dac72d8909d6353cc685358c78aa3389709d
alaced <58386621+alaced@users.noreply.github.com>
2020-11-06 18:07:24 +0900

    day10 random number game
    201014
    generate a random number according to the range input and make user guess that number with vanillaJS

---
+++ day10/index.js
@@ -0,0 +1,43 @@
+const range = document.querySelector(".js-range");
+const submit = document.querySelector(".js-play");
+const guessInput = document.querySelector(".js-guess");
+const rangeInfo = document.querySelector(".js-rangeInfo");
+const result = document.querySelector(".js-result");
+
+function paintRange() {
+  const userRange = range.value;
+  rangeInfo.innerText = `Generate a number between 0 and ${userRange}`;
+}
+
+function paintResult() {
+  const userRange = range.value;
+  const randomNumber = Math.floor(Math.random() * userRange) + 1;
+  const userNumber = guessInput.value;
+  const winOrLost = document.createElement("div");
+  winOrLost.classList.add("win-or-lost");
+  if (userNumber === "") {
+    alert("Please enter your number");
+  } else if (parseInt(userRange) < parseInt(userNumber)) {
+    result.innerText = `Please enter the number below ${userRange}`;
+  } else if (parseInt(guessInput.value) === randomNumber) {
+    winOrLost.innerText = `You won!`;
+    result.innerText = `You chose: ${userNumber}, the machine chose: ${randomNumber}.`;
+    result.appendChild(winOrLost);
+  } else {
+    winOrLost.innerText = `You lost!`;
+    result.innerText = `You chose: ${userNumber}, the machine chose: ${randomNumber}.`;
+    result.appendChild(winOrLost);
+  }
+}
+
+function submitHandler(event) {
+  event.preventDefault();
+  paintResult();
+}
+
+function init() {
+  range.addEventListener("input", paintRange);
+  submit.addEventListener("click", submitHandler);
+}
+
+init();
